20c881c303793b9f400afd33e3ba6fa5faf3d087
shoaib94 <shoaibkhan1994@gmail.com>
2018-03-20 17:31:56 +0530

    Refactoring code

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Axios from 'axios';
 import logo from './Heraldic_Sun.svg.png';
 import 'materialize-css/dist/css/materialize.css';
@@ -10,38 +10,41 @@ const API_URL = 'https://api.openweathermap.org/data/2.5/forecast/daily?APPID=43
 
 class App extends Component {
 
-  constructor(props){
-      super(props);
-      this.state = {
-          "weather" : []
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            "weather": []
+        };
+    }
 
-  getWeather = () => {
-      let weather = {};
-      Axios.get(API_URL).then(response => {
-          weather = response.data;
-          this.setState({weather: weather.list});
-      })
-          .catch(err => console.log(err));
-  };
+    getWeather = () => {
+        let weather = {};
+        Axios.get(API_URL).then(response => {
+            weather = response.data;
+            this.setState({weather: weather.list});
+        })
+            .catch(err => console.log(err));
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Weather Forecast For Pune</h1>
-        </header>
-          <div>
-              {this.state.weather.length === 0 ?
-                  (<p className="App-intro">Get 5 Day Weather Forecast for Pune, MH, India<br/><button onClick={this.getWeather} className="waves-effect waves-light btn">Get Weather</button></p>) :
-                  (<WeatherList weather={this.state.weather.length === 0 ? ([]) : (this.state.weather)}/>)
-              }
-          </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Weather Forecast For Pune</h1>
+                </header>
+                <div>
+                    {this.state.weather.length === 0 ?
+                        (<p className="App-intro">Get 5 Day Weather Forecast for Pune, MH, India<br/>
+                            <button onClick={this.getWeather} className="waves-effect waves-light btn">Get Weather
+                            </button>
+                        </p>) :
+                        (<WeatherList weather={this.state.weather.length === 0 ? ([]) : (this.state.weather)}/>)
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
--- client/src/WeatherList.js
+++ client/src/WeatherList.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Weather from './Weather';
 
 const WeatherList = (props) => {
-    console.log(props.weather);
-    return(
-        <ul className="collection" style={{"textAlign":"left","marginTop":"0px","marginBottom":"0px"}}>
-            {props.weather.map((item, i) => <Weather value={item} key={item.dt}/>)}
+    return (
+        <ul className="collection" style={{"textAlign": "left", "marginTop": "0px", "marginBottom": "0px"}}>
+            {props.weather.map(item => <Weather value={item} key={item.dt}/>)}
         </ul>
     );
 };
